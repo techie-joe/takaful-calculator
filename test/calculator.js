865b23200ae0db150e40b96abb78af2188eabc32
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2025-02-08 21:11:06 +0800

    style: enhance input styles for text and number fields, improve slider component structure

--- test/calculator.js
+++ test/calculator.js
@@ -4,7 +4,202 @@
   var
     err = function (err) { return c.error(`Calculator Error: ${err}`) },
     log = c.log,
-    _ = undefined;
+    xxx = undefined;
+
+  function blink(opt, content) {
+    return m('a._btnlink._smaller', opt, content);
+  }
+
+  function itext(opt, content) {
+    return m('i._itext', m('i.itext', opt, content));
+  }
+
+  function inRange(val, min, max) {
+    if (max < min) {
+      let _min = min;
+      min = max;
+      max = _min;
+    }
+    if (val) {
+      if (min) { val = val < min ? min : val; }
+      if (max) { val = val > max ? max : val; }
+    } else {
+      val = min ? min : max ? max : val;
+    }
+    // log(min, max, val)
+    return { val, min, max }
+  }
+
+  var SliderX = {
+    view: function (vnode) {
+      var _id = vnode.attrs.id;
+      var _min = vnode.attrs.min;
+      var _max = vnode.attrs.max;
+      var _value = vnode.attrs.value;
+      var output = m('div.c-slider-display', { id: _id + '_display', title: _id }, `${_value}`);
+      var input = m('input.c-slider-input', { type: 'range', title: _id, id: _id, min: _min, max: _max, value: _value, });
+      return m('div._p.c-full-slider', [output, input])
+    }
+  }
+
+  // ===========================================================
+
+  function Slider(initial_vnode) {
+
+    var { val, min, max } = inRange(
+      initial_vnode.attrs.value,
+      initial_vnode.attrs.min,
+      initial_vnode.attrs.max,
+    );
+
+    function decrement() { val = (val > min) ? (val - 1) : min; val = val > max ? max : val; }
+    function increment() { val = (val < max) ? (val + 1) : max; val = val < min ? min : val; }
+
+    return {
+      view: function (vnode) {
+        return m('table.c-slider', m('tr', [
+          // m('td.c-slider-value', val),
+          m('td.c-count-value', m('input.c-input', {
+            type: 'number', value: val,
+            oncreate: function (_vnode) {
+              _vnode.dom.addEventListener('change', function (e) {
+                let v = _vnode.dom.value;
+                v = isNaN(v) ? val : Number(v);
+                val = v < min ? min : v > max ? max : v;
+                m.redraw();
+              });
+            },
+          })),
+          m('td', m('button', { onclick: decrement }, '-')),
+          m('td', m('button', { onclick: increment }, '+')),
+          m('td', { style: 'width:100%' }, m('input', {
+            type: 'range',
+            class: 'c-slider-input',
+            title: initial_vnode.attrs.title,
+            min: min, max: max, value: val,
+            onchange: function (e) { val = Number(e.target.value); },
+            oncreate: function (_vnode) {
+              _vnode.dom.addEventListener('input', function (e) {
+                val = Number(e.target.value);
+                m.redraw();
+              });
+            },
+          }))
+        ]))
+      }
+    }
+  }
+
+  function Count(initial_vnode) {
+
+    var { val, min, max } = inRange(
+      initial_vnode.attrs.value,
+      initial_vnode.attrs.min,
+      initial_vnode.attrs.max,
+    );
+
+    function decrement() { val = (val > min) ? (val - 1) : min; val = val > max ? max : val; }
+    function increment() { val = (val < max) ? (val + 1) : max; val = val < min ? min : val; }
+
+    return {
+      view: function (vnode) {
+        return m('table.c-count._inline-block', m('tr', [
+          // m('td.c-count-value', val),
+          m('td.c-count-value', m('input.c-input', {
+            type: 'number', min: min, max: max, value: val,
+            oncreate: function (_vnode) {
+              _vnode.dom.addEventListener('change', function (e) {
+                let v = _vnode.dom.value;
+                v = isNaN(v) ? val : Number(v);
+                val = v < min ? min : v > max ? max : v;
+                m.redraw();
+              });
+            },
+          })),
+          m('td', m('button', { onclick: decrement }, '-')),
+          m('td', m('button', { onclick: increment }, '+')),
+        ]));
+      }
+    }
+  }
+
+  function InputNumber(initial_vnode) {
+
+    var { val, min, max } = inRange(
+      initial_vnode.attrs.value,
+      initial_vnode.attrs.min,
+      initial_vnode.attrs.max,
+    );
+
+    return {
+      view: function (vnode) {
+        return m('input.c-input', {
+          type: 'number', min: min, max: max, value: val,
+          oncreate: function (_vnode) {
+            _vnode.dom.addEventListener('change', function (e) {
+              let v = _vnode.dom.value;
+              v = isNaN(v) ? val : Number(v);
+              val = v < min ? min : v > max ? max : v;
+              m.redraw();
+            });
+          },
+        });
+      }
+    }
+  }
+
+  function Calculator(initial_vnode) {
+
+    var testVal = 100;
+    var countVal = 100;
+    var sliderVal = 10;
+
+    return {
+      view: function () {
+        return [
+          m('div.c-display._pa._radius-x25r', [
+            m('a.c-close._btnlink._small._no-pad._lh-0', { href: '#!/hello', title: 'Close Calculator' }, itext('×')),
+            m('h3._h6._no-margin', 'Takaful Calculator'), m('hr'),
+            m(SliderX, { id: 'sliderValueX', min: 0, max: 100, value: sliderVal }),
+            m('div', [
+              m('span.c-row-label', 'Slider'),
+              m(Slider, { id: 'sliderValue', min: 0, max: 100, value: sliderVal }),
+            ]),
+            m('div', [
+              m('span.c-row-label', 'Count'),
+              m(Count, { min: 0, max: 200, value: countVal }),
+            ]),
+            m('div', [
+              m('span.c-row-label', 'Test'),
+              m('table.c-count._inline-block', m('tr', [
+                m('td.c-count-value', testVal),
+                m('input', {
+                  type: 'number', min: 0, max: 99999, value: testVal,
+                  oncreate: function (_vnode) {
+                    _vnode.dom.addEventListener('change', function (e) {
+                      let v = _vnode.dom.value;
+                      testVal = isNaN(v) ? v : Number(v);
+                      m.redraw();
+                    });
+                  },
+                }),
+              ])),
+            ]),
+            m('hr'),
+            blink({ href: '#!/hello' }, 'Close'), ' ',
+          ]),
+        ]
+      }
+    }
+  };
+
+  var Hello = {
+    view: function () {
+      return m('div.c-display._pa._radius-x25r', [
+        m('a._btnlink._small', { href: '#!/calculator' }, 'Open Calculator'),
+      ]);
+    }
+  };
 
   var Example = {
 
@@ -35,171 +230,6 @@
     // }
 
   }
-
-  var SliderX = {
-    view: function (vnode) {
-      var _id = vnode.attrs.id;
-      var _min = vnode.attrs.min;
-      var _max = vnode.attrs.max;
-      var _value = vnode.attrs.value;
-      var _onchange = vnode.attrs.onchange;
-      var output = m('div.c-slider-display', { id: _id + '_display', title: _id }, `${_value}`);
-      var input = m('input.c-slider-input', {
-        type: 'range', title: _id, id: _id, min: _min, max: _max, value: _value,
-        onchange: function (e) {
-          sliderValue = input.dom.value;
-        },
-        oncreate: function (vnode) {
-          vnode.dom.addEventListener('input', function () {
-            output.dom.textContent = input.dom.value;
-          });
-        },
-      });
-      return m('div._p.c-full-slider', [output,input])
-    }
-  }
-
-  var sliderValue = 15;
-
-  function reset() {
-    sliderValue = 15;
-  }
-
-  var Hello = {
-    view: function () {
-      return m('div.c-display._pa._radius-x25r', [
-        m('a._btnlink._small', { href: '#!/calculator', onclick: reset }, 'Open Calculator'),
-      ]);
-    }
-  };
-
-  function blink(opt, content) {
-    return m('a._btnlink._smaller', opt, content);
-  }
-
-  function itext(opt, content) {
-    return m('i._itext', m('i.itext', opt, content));
-  }
-
-  function inRange(val, min, max) {
-    if (max < min) {
-      let _min = min;
-      min = max;
-      max = _min;
-    }
-    if (val) {
-      if (min) { val = val < min ? min : val; }
-      if (max) { val = val > max ? max : val; }
-    } else {
-      val = min ? min : max ? max : val;
-    }
-    // log(min, max, val)
-    return { val, min, max }
-  }
-
-  // ===========================================================
-
-  var countVal = 10;
-  var sliderVal = 10;
-
-  function Slider(initial_vnode) {
-
-    var { val, min, max } = inRange(
-      initial_vnode.attrs.value,
-      initial_vnode.attrs.min,
-      initial_vnode.attrs.max,
-    );
-
-    function increment() { (val < max) ? (val += 1) : (val = max); val = val < min ? min : val; }
-    function decrement() { (val > min) ? (val -= 1) : (val = min); val = val > max ? max : val; }
-
-    return {
-      view: function (vnode) {
-        return m('table.c-slider', m('tr', [
-          // m('td.c-slider-value', val),
-          m('td.c-count-value', m('input.c-input', {
-            type: 'number', value: val,
-            oncreate: function (_vnode) {
-              _vnode.dom.addEventListener('change', function (e) {
-                val = Number(_vnode.dom.value) || val;
-                val = val < min ? min : val > max ? max : val;
-                m.redraw();
-              });
-            },
-          })),
-          m('td', m('button', { onclick: decrement }, '-')),
-          m('td', m('button', { onclick: increment }, '+')),
-          m('td', { style: 'width:100%' }, m('input', {
-            type: 'range',
-            class: 'c-slider-input',
-            title: initial_vnode.attrs.title,
-            min: min, max: max, value: val,
-            onchange: function (e) { sliderValue = e.target.value; },
-            oncreate: function (_vnode) {
-              _vnode.dom.addEventListener('input', function (e) {
-                val = e.target.value;
-                m.redraw();
-              });
-            },
-          }))
-        ]))
-      }
-    }
-  }
-
-  function Count(initial_vnode) {
-
-    var { val, min, max } = inRange(
-      initial_vnode.attrs.value,
-      initial_vnode.attrs.min,
-      initial_vnode.attrs.max,
-    );
-
-    function increment() { (val < max) ? (val += 1) : (val = max); val = val < min ? min : val; }
-    function decrement() { (val > min) ? (val -= 1) : (val = min); val = val > max ? max : val; }
-
-    return {
-      view: function (vnode) {
-        return m('table.c-count._inline-block', m('tr', [
-          // m('td.c-count-value', val),
-          m('td.c-count-value', m('input.c-input', {
-            type: 'number', value: val,
-            oncreate: function (_vnode) {
-              _vnode.dom.addEventListener('change', function (e) {
-                val = Number(_vnode.dom.value) || val;
-                val = val < min ? min : val > max ? max : val;
-                m.redraw();
-              });
-            },
-          })),
-          m('td', m('button', { onclick: decrement }, '-')),
-          m('td', m('button', { onclick: increment }, '+')),
-        ]));
-      }
-    }
-  }
-
-  var Calculator = {
-    view: function () {
-      return [
-        m('div.c-display._pa._radius-x25r', [
-          m('a.c-close._btnlink._small._no-pad._lh-0', { href: '#!/hello', title: 'Close Calculator' }, itext('×')),
-          m('h3._h6._no-margin', 'Takaful Calculator'), m('hr'),
-          m(SliderX, { id: 'sliderValueX', min: 0, max: 100, value: sliderValue }),
-          m('div', [
-            m('span.c-row-label', 'Slider'),
-            m(Slider, { id: 'sliderValue', min: 0, max: 100, value: sliderVal }),
-          ]),
-          m('div', [
-            m('span.c-row-label', 'Count'),
-            m(Count, { min: 0, max: 20, value: countVal }),
-          ]),
-          m('hr'),
-          blink({ href: '#!/hello' }, 'Close'), ' ',
-        ]),
-      ]
-    }
-  };
 
   function loadAppTo(e) {
     m.route(e, '/calculator', {
